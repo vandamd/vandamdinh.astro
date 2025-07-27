6b7478c4e161c9783cf9587c37c8e4d650738005
vandamd <vandamdinh71@gmail.com>
2025-07-27 23:08:51 +0100

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,11 @@ import keystatic from '@keystatic/astro'
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
+        resolve: {
+            alias: import.meta.env.PROD && {
+                "react-dom/server": "react-dom/server.edge",
+            }
+        }
     },
 
     image: {
